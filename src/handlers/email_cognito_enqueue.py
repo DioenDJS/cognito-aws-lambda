7f1fbb2c3df846bff6c7018a1dc087a973a02b1d
DioenDJS <negociospacheti@gmail.com>
2024-08-10 23:29:20 -0300

    :tada: Init project

---
+++ src/handlers/email_cognito_enqueue.py
@@ -0,0 +1,18 @@
+import json
+import structlog
+
+def lambda_handler(event, context):
+    # TODO implement
+    logger = structlog.get_logger()
+    logger.info(event)
+    logger.info(context)
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Content-Type": "application/json",  # Corrected typo
+        },
+        'body': json.dumps({
+            "name": "Dioane",
+            "email": "diovane2gmail.com"
+        })
+    }
